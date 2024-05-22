e5542af8a4c099cb0ac364f9841382de56e83764
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-05-22 12:54:51 +0300

    Fixing task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -36,7 +36,7 @@ def unauthorized() -> str:
     abort(401)
 
 
-@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
 def forbidden() -> str:
     """GET /api/v1/forbidden
     Return:
